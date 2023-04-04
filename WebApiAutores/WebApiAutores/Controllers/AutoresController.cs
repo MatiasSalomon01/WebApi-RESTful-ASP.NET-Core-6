0a0a20289a67fdc6a78ea4f56d08bb7ff5859da7
=Matias Salomon <matias.salomon@clt.com.py>
2023-04-04 11:52:23 -0400

    Actualizando y Borrando Recursos

--- WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -28,5 +28,25 @@ namespace WebApiAutores.Controllers
             await _context.SaveChangesAsync();
             return Ok();
         }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            if (autor.Id != id) return BadRequest("El id del autor no coincide con la URL");
+
+            _context.Autores.Update(autor);
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var result = await _context.Autores.AnyAsync(a => a.Id == id);
+            if (!result) return NotFound();
+
+            _context.Remove(new Autor() { Id = id});
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
