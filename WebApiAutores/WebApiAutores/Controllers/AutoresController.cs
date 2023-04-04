9a6f2d38e635a8660ed26a5b0d30a4576bc101d5
=Matias Salomon <matias.salomon@clt.com.py>
2023-04-04 10:56:59 -0400

    Iniciando el Desarrollo del Web API

---
+++ WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor() { Id = 1, Nombre = "Felipe"},
+                new Autor() { Id = 1, Nombre = "Felipe"}
+            };
+        }
+    }
+}
---
+++ WebApiAutores/WebApiAutores/Entidades/Autor.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiAutores.Entidades
+{
+    public class Autor
+    {
+        public int Id{ get; set; }
+        public string Nombre { get; set; }
+
+    }
+}                                 
+                                  
