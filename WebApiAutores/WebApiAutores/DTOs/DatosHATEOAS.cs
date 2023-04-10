0f5dc6367e07f06adbebff22bb82148931702e22
=Matias Salomon <matias.salomon@clt.com.py>
2023-04-10 16:43:54 -0400

    Implementando HATEOAS

---
+++ WebApiAutores/WebApiAutores/DTOs/DatosHATEOAS.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApiAutores.DTOs
+{
+    public class DatosHATEOAS
+    {
+        public string Enlace{ get; private set; }
+        public string Descripcion{ get; private set; }
+        public string Metodo{ get; private set; }
+
+        public DatosHATEOAS(string enlace, string descripcion, string metodo)
+        {
+            Enlace = enlace;
+            Descripcion = descripcion;
+            Metodo = metodo;
+        }
+    }
+}
