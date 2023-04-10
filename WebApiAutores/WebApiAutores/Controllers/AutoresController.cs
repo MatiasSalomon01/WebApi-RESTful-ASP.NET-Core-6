df2b9068e455bdb25525bc27c23ad127bd396e2d
=Matias Salomon <matias.salomon@clt.com.py>
2023-04-10 11:33:11 -0400

    Ejemplo de IConfiguration

--- WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -12,11 +12,19 @@ namespace WebApiAutores.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
 
-        public AutoresController(ApplicationDbContext context, IMapper mapper)
+        public AutoresController(ApplicationDbContext context, IMapper mapper, IConfiguration configuration)
         {
             _context = context;
             _mapper = mapper;
+            _configuration = configuration;
+        }
+
+        [HttpGet("get-config")]
+        public ActionResult<string> GetConfig()
+        {
+            return _configuration["ConnectionStrings:DefaultConnection"];
         }
 
         [HttpGet]
