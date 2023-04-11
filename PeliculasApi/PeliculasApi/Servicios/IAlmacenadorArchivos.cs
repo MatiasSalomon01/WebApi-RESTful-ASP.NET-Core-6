684534cf9d1896fcddf8611fbc6a2cf8424e1fd9
=Matias Salomon <matias.salomon@clt.com.py>
2023-04-11 16:59:25 -0400

    Creacion de Servicio para subir fotos

---
+++ PeliculasApi/PeliculasApi/Servicios/IAlmacenadorArchivos.cs
@@ -0,0 +1,9 @@
+﻿namespace PeliculasApi.Servicios
+{
+    public interface IAlmacenadorArchivos
+    {
+        Task<string> GuardarArchivo(byte[] contenido, string extension, string contenedor, string contentType);
+        Task<string> EditarArchivo(byte[] contenido, string extension, string contenedor, string contentType);
+        Task<string> BorrarArchivo(string ruta, string contenedor);
+    }
+}
