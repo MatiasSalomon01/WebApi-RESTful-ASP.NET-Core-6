70d5613b16e0c1cea00ced4da378317ef203abed
=Matias Salomon <matias.salomon@clt.com.py>
2023-04-12 14:25:13 -0400

    SeedData

--- PeliculasApi/PeliculasApi/ApplicationDbContext.cs
+++ PeliculasApi/PeliculasApi/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using PeliculasApi.Entidades;
 
 namespace PeliculasApi
@@ -17,7 +18,101 @@ namespace PeliculasApi
             modelBuilder.Entity<PeliculaGenero>()
                 .HasKey(x => new { x.GeneroId, x.PeliculaId });
 
+            SeedData(modelBuilder);
+
             base.OnModelCreating(modelBuilder);
+        }
+
+        private void SeedData(ModelBuilder modelBuilder)
+        {
+            var aventura = new Genero() { Id = 4, Nombre = "Aventura" };
+            var animation = new Genero() { Id = 5, Nombre = "Animación" };
+            var suspenso = new Genero() { Id = 6, Nombre = "Suspenso" };
+            var romance = new Genero() { Id = 7, Nombre = "Romance" };
+
+            modelBuilder.Entity<Genero>()
+                .HasData(new List<Genero>
+                {
+                    aventura, animation, suspenso, romance
+                });
+
+            var jimCarrey = new Actor() { Id = 5, Nombre = "Jim Carrey", FechaNacimiento = new DateTime(1962, 01, 17) };
+            var robertDowney = new Actor() { Id = 6, Nombre = "Robert Downey Jr.", FechaNacimiento = new DateTime(1965, 4, 4) };
+            var chrisEvans = new Actor() { Id = 7, Nombre = "Chris Evans", FechaNacimiento = new DateTime(1981, 06, 13) };
+
+            modelBuilder.Entity<Actor>()
+                .HasData(new List<Actor>
+                {
+                    jimCarrey, robertDowney, chrisEvans
+                });
+
+            var endgame = new Pelicula()
+            {
+                Id = 2,
+                Titulo = "Avengers: Endgame",
+                EnCines = true,
+                FechaEstreno = new DateTime(2019, 04, 26)
+            };
+
+            var iw = new Pelicula()
+            {
+                Id = 3,
+                Titulo = "Avengers: Infinity Wars",
+                EnCines = false,
+                FechaEstreno = new DateTime(2019, 04, 26)
+            };
+
+            var sonic = new Pelicula()
+            {
+                Id = 4,
+                Titulo = "Sonic the Hedgehog",
+                EnCines = false,
+                FechaEstreno = new DateTime(2020, 02, 28)
+            };
+            var emma = new Pelicula()
+            {
+                Id = 5,
+                Titulo = "Emma",
+                EnCines = false,
+                FechaEstreno = new DateTime(2020, 02, 21)
+            };
+            var wonderwoman = new Pelicula()
+            {
+                Id = 6,
+                Titulo = "Wonder Woman 1984",
+                EnCines = false,
+                FechaEstreno = new DateTime(2020, 08, 14)
+            };
+
+            modelBuilder.Entity<Pelicula>()
+                .HasData(new List<Pelicula>
+                {
+                    endgame, iw, sonic, emma, wonderwoman
+                });
+
+            modelBuilder.Entity<PeliculaGenero>().HasData(
+                new List<PeliculaGenero>()
+                {
+                    new PeliculaGenero(){PeliculaId = endgame.Id, GeneroId = suspenso.Id},
+                    new PeliculaGenero(){PeliculaId = endgame.Id, GeneroId = aventura.Id},
+                    new PeliculaGenero(){PeliculaId = iw.Id, GeneroId = suspenso.Id},
+                    new PeliculaGenero(){PeliculaId = iw.Id, GeneroId = aventura.Id},
+                    new PeliculaGenero(){PeliculaId = sonic.Id, GeneroId = aventura.Id},
+                    new PeliculaGenero(){PeliculaId = emma.Id, GeneroId = suspenso.Id},
+                    new PeliculaGenero(){PeliculaId = emma.Id, GeneroId = romance.Id},
+                    new PeliculaGenero(){PeliculaId = wonderwoman.Id, GeneroId = suspenso.Id},
+                    new PeliculaGenero(){PeliculaId = wonderwoman.Id, GeneroId = aventura.Id},
+                });
+
+            modelBuilder.Entity<PeliculaActor>().HasData(
+                new List<PeliculaActor>()
+                {
+                    new PeliculaActor(){PeliculaId = endgame.Id, ActorId = robertDowney.Id, Personaje = "Tony Stark", Orden = 1},
+                    new PeliculaActor(){PeliculaId = endgame.Id, ActorId = chrisEvans.Id, Personaje = "Steve Rogers", Orden = 2},
+                    new PeliculaActor(){PeliculaId = iw.Id, ActorId = robertDowney.Id, Personaje = "Tony Stark", Orden = 1},
+                    new PeliculaActor(){PeliculaId = iw.Id, ActorId = chrisEvans.Id, Personaje = "Steve Rogers", Orden = 2},
+                    new PeliculaActor(){PeliculaId = sonic.Id, ActorId = jimCarrey.Id, Personaje = "Dr. Ivo Robotnik", Orden = 1}
+                });
         }
 
         public DbSet<Genero> Generos { get; set; }
---
+++ PeliculasApi/PeliculasApi/Migrations/20230412182251_SeedData.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PeliculasApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Actores",
+                columns: new[] { "Id", "FechaNacimiento", "Foto", "Nombre" },
+                values: new object[,]
+                {
+                    { 5, new DateTime(1962, 1, 17, 0, 0, 0, 0, DateTimeKind.Utc), null, "Jim Carrey" },
+                    { 6, new DateTime(1965, 4, 4, 0, 0, 0, 0, DateTimeKind.Utc), null, "Robert Downey Jr." },
+                    { 7, new DateTime(1981, 6, 13, 0, 0, 0, 0, DateTimeKind.Utc), null, "Chris Evans" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Generos",
+                columns: new[] { "Id", "Nombre" },
+                values: new object[,]
+                {
+                    { 4, "Aventura" },
+                    { 5, "Animación" },
+                    { 6, "Suspenso" },
+                    { 7, "Romance" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Peliculas",
+                columns: new[] { "Id", "EnCines", "FechaEstreno", "Poster", "Titulo" },
+                values: new object[,]
+                {
+                    { 2, true, new DateTime(2019, 4, 26, 0, 0, 0, 0, DateTimeKind.Utc), null, "Avengers: Endgame" },
+                    { 3, false, new DateTime(2019, 4, 26, 0, 0, 0, 0, DateTimeKind.Utc), null, "Avengers: Infinity Wars" },
+                    { 4, false, new DateTime(2020, 2, 28, 0, 0, 0, 0, DateTimeKind.Utc), null, "Sonic the Hedgehog" },
+                    { 5, false, new DateTime(2020, 2, 21, 0, 0, 0, 0, DateTimeKind.Utc), null, "Emma" },
+                    { 6, false, new DateTime(2020, 8, 14, 0, 0, 0, 0, DateTimeKind.Utc), null, "Wonder Woman 1984" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PeliculasActores",
+                columns: new[] { "ActorId", "PeliculaId", "Orden", "Personaje" },
+                values: new object[,]
+                {
+                    { 5, 4, 1, "Dr. Ivo Robotnik" },
+                    { 6, 2, 1, "Tony Stark" },
+                    { 6, 3, 1, "Tony Stark" },
+                    { 7, 2, 2, "Steve Rogers" },
+                    { 7, 3, 2, "Steve Rogers" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PeliculasGeneros",
+                columns: new[] { "GeneroId", "PeliculaId" },
+                values: new object[,]
+                {
+                    { 4, 2 },
+                    { 4, 3 },
+                    { 4, 4 },
+                    { 4, 6 },
+                    { 6, 2 },
+                    { 6, 3 },
+                    { 6, 5 },
+                    { 6, 6 },
+                    { 7, 5 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Generos",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasActores",
+                keyColumns: new[] { "ActorId", "PeliculaId" },
+                keyValues: new object[] { 5, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasActores",
+                keyColumns: new[] { "ActorId", "PeliculaId" },
+                keyValues: new object[] { 6, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasActores",
+                keyColumns: new[] { "ActorId", "PeliculaId" },
+                keyValues: new object[] { 6, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasActores",
+                keyColumns: new[] { "ActorId", "PeliculaId" },
+                keyValues: new object[] { 7, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasActores",
+                keyColumns: new[] { "ActorId", "PeliculaId" },
+                keyValues: new object[] { 7, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 4, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 4, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 4, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 4, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 6, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 6, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 6, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 6, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "PeliculasGeneros",
+                keyColumns: new[] { "GeneroId", "PeliculaId" },
+                keyValues: new object[] { 7, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "Actores",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Actores",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Actores",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Generos",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Generos",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Generos",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Peliculas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Peliculas",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Peliculas",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Peliculas",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Peliculas",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
